7fbdbb9c9797b7365e0405e564c29f5d0c251197
wangwei <wangwei@timeface.cn>
2018-01-12 14:18:29 +0800

    添加配置文件和项目

---
+++ app/src/main/java/com/example/wangwei/mypaintview/view/MyCustomView.java
@@ -0,0 +1,60 @@
+package com.example.wangwei.mypaintview.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by wangwei on 2017/7/6.
+ */
+
+public class MyCustomView extends View {
+    Paint paint;
+
+    public MyCustomView(Context context) {
+        super(context);
+    }
+
+    public MyCustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        paint = new Paint();
+    }
+
+    public MyCustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MyCustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(200,200);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        paint.setColor(Color.YELLOW);
+        canvas.drawRect(200,200,getWidth(),getHeight(),paint);
+        paint.setColor(Color.BLUE);
+        paint.setTextSize(20);
+        paint.getTextBounds("s",0,0,new Rect());
+        Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+        float v = fontMetrics.descent - fontMetrics.ascent;
+        canvas.drawText("ss",0,getHeight()/2,paint);
+    }
+}
---
+++ app/src/main/java/com/example/wangwei/mypaintview/view/MyListView.java
@@ -0,0 +1,58 @@
+package com.example.wangwei.mypaintview.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ListView;
+
+/**
+ * Created by wangwei on 2017/7/11.
+ */
+
+public class MyListView extends ListView implements View.OnClickListener,GestureDetector.OnGestureListener {
+
+    public MyListView(Context context) {
+        super(context);
+    }
+
+    public MyListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
